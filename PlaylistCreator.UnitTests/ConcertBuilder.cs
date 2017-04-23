52dd9b5a60127420aee5f0b87b031522e9e08813
MichaelEdk <michael.edkins@gmail.com>
2017-04-23 17:29:56 +1200

    Initial commit stage four

---
+++ PlaylistCreator.UnitTests/ConcertBuilder.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Models;
+
+namespace PlaylistCreator.UnitTests
+{
+    internal interface IConcertBuilder
+    {
+        IConcert Build();
+        IConcertBuilder WithArtist(string artist);
+        IConcertBuilder WithConcertDate(DateTime concertDate);
+        IConcertBuilder WithTitle(string title);
+        IConcertBuilder WithTour(string tour);
+    }
+
+    internal sealed class ConcertBuilder : IConcertBuilder
+    {
+        private string _artist;
+        private DateTime _concertDate;
+        private string _title;
+        private string _tour;
+
+        public IConcert Build()
+        {
+            return new Concert();
+            //return new Concert(
+            //    _title,
+            //    _artist,
+            //    _tour,
+            //    _concertDate.Month.ToString(),
+            //    _concertDate.Day.ToString(),
+            //    _concertDate.Year.ToString()
+            //    );
+        }
+
+        public IConcertBuilder WithArtist(string artist)
+        {
+            _artist = artist;
+            return this;
+        }
+
+        public IConcertBuilder WithConcertDate(DateTime concertDate)
+        {
+            _concertDate = concertDate;
+            return this;
+        }
+
+        public IConcertBuilder WithTitle(string title)
+        {
+            _title = title;
+            return this;
+        }
+
+        public IConcertBuilder WithTour(string tour)
+        {
+            _tour = tour;
+            return this;
+        }
+    }
+}
---
+++ PlaylistCreator.UnitTests/SongBuilder.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections.Generic;
+using Models;
+
+namespace PlaylistCreator.UnitTests
+{
+    public interface ISongBuilder
+    {
+        ISong Build();
+        ISongBuilder WithTitle(string title);
+        ISongBuilder WithMinutes(int minutes);
+        ISongBuilder WithArtist(string artist);
+        ISongBuilder WithAlbum(string album);
+        ISongBuilder WithUri(string uri);
+    }
+
+    public class SongBuilder : ISongBuilder
+    {
+        private string _title;
+        private int _minutes;
+        private readonly IList<string> _artists = new List<string>();
+        private string _album;
+        private string _uri;
+
+        public ISong Build()
+        {
+            return new Song(
+                _title,
+                _minutes,
+                _artists,
+                _album,
+                _uri);
+        }
+
+        public ISongBuilder WithTitle(string title)
+        {
+            _title = title;
+            return this;
+        }
+
+        public ISongBuilder WithMinutes(int minutes)
+        {
+            _minutes = minutes;
+            return this;
+        }
+
+        public ISongBuilder WithArtist(string artist)
+        {
+            _artists.Add(artist);
+            return this;
+        }
+
+        public ISongBuilder WithAlbum(string album)
+        {
+            _album = album;
+            return this;
+        }
+
+        public ISongBuilder WithUri(string uri)
+        {
+            _uri = uri;
+            return this;
+        }
+    }
+}
